78ff1ca4cca7232535132da892e45f4aef33f90b
akshay0077 <kherakshay007@gmail.com>
2022-02-26 23:59:47 +0530

    26th feb 2022

--- demo.cpp
+++ demo.cpp
@@ -1,12 +1,7 @@
-(1) To Check Actual Ram on Laptop
- -> grep MemTotal /proc/meminfo
+#include <iostream>
+using namespace std;
 
- (2) To Check a Maximum Ram Size in Over laptop
- -> sudo dmidecode -t 16
-
- (3) To Check Type (DDR4)/Speed 
- -> sudo dmidecode -t 17
-
- (4) To check Ram capacity 
- -> sudo dmidecode -t memory | grep -Po '^\tMaximum Capacity: \K.*'
-
+int main(){
+	cout<<"Great Back ho here";
+	return 0;
+}
